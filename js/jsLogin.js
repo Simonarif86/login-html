064d0dcb72c0cba2835c86a7c1d9706f6ff3aed4
simonarif86 <simonarif86@gmail.com>
2025-03-27 10:25:15 +0700

    login html tahap 1

---
+++ js/jsLogin.js
@@ -0,0 +1,25 @@
+ // Function to handle the form submission
+ function handleFormSubmit() {
+    // Get the input values
+    var email = document.getElementById('exampleInputEmail1').value;
+    var password = document.getElementById('exampleInputPassword1').value;
+    var message = document.getElementById('message').value;
+
+    var passDefault = "sugeng";  // Predefined password for validation
+
+    var selectedRadio = '';
+    if (document.getElementById('radio1').checked) {
+        selectedRadio = 'RADIO1';
+    } else if (document.getElementById('radio2').checked) {
+        selectedRadio = 'RADIO2';
+    }
+
+    // Validate the password
+    if (passDefault === password) {
+        // Show an alert with form data if password is correct
+        alert('Email: ' + email + '\nPassword: ' + 'Password Benar' + '\nMessage: ' + message + '\nSelected Radio: ' + selectedRadio);
+    } else {
+        // Show an error alert if password is incorrect
+        alert('Email: ' + email + '\nPassword: ' + 'Password Salah' + '\nMessage: ' + message + '\nSelected Radio: ' + selectedRadio);
+    }
+}
